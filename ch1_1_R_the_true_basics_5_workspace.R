6a11686eea5fa2621a4d3f9f549cedb085e234c0
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 09:09:26 -0800

    Added workspace R script.

---
+++ ch1_1_R_the_true_basics_5_workspace.R
@@ -0,0 +1,23 @@
+# Clear the entire workspace
+rm(list = ls())
+
+# List the contents of your workspace
+ls()
+
+# Create the variable horses
+horses <- 3
+
+# Create the variable dogs
+dogs <- 7
+
+# Create the variable animals
+animals <- horses + dogs
+
+# Inspect the contents of the workspace again
+ls()
+
+# Remove dogs from the workspace
+rm(dogs)
+
+# Inspect the objects in your workspace once more
+ls()
