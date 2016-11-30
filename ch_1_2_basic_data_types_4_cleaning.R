12d6c3de84e1a1aef29d8f9ba713fbc09e041dc1
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 12:07:35 -0800

    Added cleaning script.

---
+++ ch_1_2_basic_data_types_4_cleaning.R
@@ -0,0 +1,8 @@
+# Convert age to numeric: age_clean
+age_clean <- as.numeric(age)
+
+# Convert employed to logical: employed_clean
+employed_clean <- as.logical(employed)
+
+# Convert salary to numeric: salary_clean
+salary_clean <- as.numeric(salary)
