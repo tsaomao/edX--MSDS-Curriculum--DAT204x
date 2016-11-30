29e34c94ef2e03e0e8217364e3d5f267d15e0d2b
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 11:58:57 -0800

    Two new scripts for second part of chapter 1.

---
+++ ch_1_2_basic_data_types_1_script.R
@@ -0,0 +1,8 @@
+# What is the answer to the universe?
+my_numeric <- 42
+
+# The quotation marks indicate that the variable is of type character
+my_character <- "forty-two"
+
+# Change the value of my_logical
+my_logical <- FALSE
---
+++ ch_1_2_basic_data_types_2_apples_and_oranges.R
@@ -0,0 +1,13 @@
+# Assign a value to the variable called my_apples
+my_apples <- 5 
+
+# Print out the value of my_apples
+my_apples       
+
+# Assign a value to the variable my_oranges and print it out
+my_oranges <- 6
+my_oranges 
+
+# New variable that contains the total amount of fruit
+my_fruit <- my_apples + my_oranges 
+my_fruit
