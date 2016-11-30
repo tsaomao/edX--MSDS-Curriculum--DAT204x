318caf4012854912b7071705a0c32062376b2d4d
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 13:53:59 -0800

    Adding and subtracting vectors (element-wise).

---
+++ ch_2_2_vector_arithmetic_1_summing_and_subtracting.R
@@ -0,0 +1,15 @@
+# A_vector and B_vector have already been defined for you
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector: total_vector
+total_vector <- A_vector + B_vector
+
+# Print total_vector
+total_vector
+
+# Calculate the difference between A_vector and B_vector: diff_vector
+diff_vector <- A_vector - B_vector
+
+# Print diff_vector
+diff_vector
