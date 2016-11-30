1d82c58d7d308884c0b89b6dc8b796ea82719643
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 11:54:21 -0800

    Added pre-step to clear other variables. Also added rm()s for r and R.

--- ch1_1_R_the_true_basics_6_build_and_destroy.R
+++ ch1_1_R_the_true_basics_6_build_and_destroy.R
@@ -1,7 +1,9 @@
+# Pre step: Clear out workspace
+rm(list = ls())
+
 # Create the variables r and R
 r <- 2
 R <- 6
-
 
 # Calculate the volume of the donut: vol_donut
 pisquared <- pi^2
@@ -11,6 +13,8 @@ vol_donut <- 2*pisquared*rsquared*R
 # Remove all intermediary variables that you've used with rm()
 rm(pisquared)
 rm(rsquared)
+rm(r)
+rm(R)
 
 # List the elements in your workspace
 ls()
