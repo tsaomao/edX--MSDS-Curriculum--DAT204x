ec69a8fda7a6a5b87610becd5a130f0d0f9c837c
Malcolm Gin <malcolm.gin@gmail.com>
2016-11-30 09:20:17 -0800

    Added build_and_destroy script file.

---
+++ ch1_1_R_the_true_basics_6_build_and_destroy.R
@@ -0,0 +1,16 @@
+# Create the variables r and R
+r <- 2
+R <- 6
+
+
+# Calculate the volume of the donut: vol_donut
+pisquared <- pi^2
+rsquared <- r^2
+vol_donut <- 2*pisquared*rsquared*R
+
+# Remove all intermediary variables that you've used with rm()
+rm(pisquared)
+rm(rsquared)
+
+# List the elements in your workspace
+ls()
