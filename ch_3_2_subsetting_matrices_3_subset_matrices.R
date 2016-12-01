38f00293f9b71545e3b0dfc0b9f07a25d7b1d413
Malcolm Gin <malcolm.gin@gmail.com>
2016-12-01 11:09:15 -0800

    Subset matrices and alternate subsets.

---
+++ ch_3_2_subsetting_matrices_3_subset_matrices.R
@@ -0,0 +1,13 @@
+# Setup
+# Star Wars box office in millions (!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+star_wars_matrix <- rbind(new_hope, empire_strikes, return_jedi)
+colnames(star_wars_matrix) <- c("US", "non-US")
+rownames(star_wars_matrix) <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+
+# star_wars_matrix is already defined in your workspace
+
+# All figures for "A New Hope" and "Return of the Jedi"
+star_wars_matrix[c(1,3), c(1,2)]
---
+++ ch_3_2_subsetting_matrices_4_alternate_subsets.R
@@ -0,0 +1,19 @@
+# Setup
+# Star Wars box office in millions (!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+star_wars_matrix <- rbind(new_hope, empire_strikes, return_jedi)
+colnames(star_wars_matrix) <- c("US", "non-US")
+rownames(star_wars_matrix) <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+
+# star_wars_matrix is already defined in your workspace
+
+# Select the US revenues for "A New Hope" and "The Empire Strikes Back"
+star_wars_matrix[c("A New Hope", "The Empire Strikes Back"), c("US", "non-US")]
+
+# Select the last two rows and both columns
+star_wars_matrix[c(FALSE, TRUE, TRUE), c(TRUE, TRUE)]
+
+# Select the non-US revenue for "The Empire Strikes Back"
+star_wars_matrix[c(FALSE, TRUE, FALSE), c(FALSE, TRUE)]
