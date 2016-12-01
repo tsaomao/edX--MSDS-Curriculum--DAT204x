b4ce59d22512e41c3f73aa079f94169223d41060
Malcolm Gin <malcolm.gin@gmail.com>
2016-12-01 08:53:55 -0800

    They wanted it the other way around (less intuitive for me, but hey.)

--- ch_3_1_create_name_matrices_1_star_wars_box.R
+++ ch_3_1_create_name_matrices_1_star_wars_box.R
@@ -2,4 +2,4 @@
 box <- c(460.998, 314.4, 290.475, 247.900, 309.306, 165.8)
 
 # Create star_wars_matrix
-star_wars_matrix <- matrix(box, ncol = 2)
+star_wars_matrix <- matrix(box, byrow = TRUE, nrow = 3)
