f5cd7146685ae9a4aad2fdfe2104da10fd34813b
Malcolm Gin <malcolm.gin@gmail.com>
2016-12-01 09:18:42 -0800

    More scripts in Chapter 3, section 1.

---
+++ ch_3_1_create_name_matrices_6_adding_a_row.R
@@ -0,0 +1,23 @@
+# Setup
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+cols <- c("US", "non-US")
+star_wars_matrix <- rbind(new_hope, empire_strikes, return_jedi)
+colnames(star_wars_matrix) <- cols
+rownames(star_wars_matrix) <- c("A New Hope", "The Empire Strikes Back", "Return of the Jedi")
+phantom_menace <- c(474.5, 552.5)
+attack_clones <- c(310.7, 338.7)
+revenge_sith <- c(380.3, 468.5)
+star_wars_matrix2 <- rbind(phantom_menace, attack_clones, revenge_sith)
+colnames(star_wars_matrix2) <- cols
+rownames(star_wars_matrix2) <- c("Phantom Menace", "Attack of the Clones", "Revenge of the Sith")
+
+# Matrix that contains the first trilogy box office
+star_wars_matrix  
+
+# Matrix that contains the second trilogy box office
+star_wars_matrix2 
+
+# Combine both Star Wars trilogies in one matrix: all_wars_matrix
+all_wars_matrix2 <- rbind(star_wars_matrix, star_wars_matrix2)
